ac14ff0002002d3a80c9f53e86752f0531253d50
Gabriele Napoli <gabriele.napoli@scuolazoo.it>
2023-08-25 14:46:33 +0200

    fix style page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,13 +13,13 @@ const Login = () => {
 
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="flex flex-col items-center gap-5a">
+      <div className="flex flex-col items-center gap-5">
         <BsTwitch className="h-12 w-12" />
         <button
           className="bg-violet-700 p-3 text-gray-50"
           onClick={() => signIn()}
         >
-          Sign in!
+          Log in with twitch!
         </button>
       </div>
     </div>
